7f56deacf3879243444e6bd5164b82194714c9ec
MarioGogogo <148373644@qq.com>
2021-07-22 12:28:22 +0800

    🎉 🎉 🤔 ch01-Variable

---
+++ ch01/main.go
@@ -0,0 +1,13 @@
+package main 
+
+import "fmt"
+
+func main() {
+	  var i int = 10
+		var x = "'100'"
+		var (
+			j = 0
+			k = true
+		)
+    fmt.Printf("i",i,x,j,k)
+}
---
+++ mylab/main.go
@@ -0,0 +1,23 @@
+package main 
+
+import "fmt"
+
+
+
+//main入口函数 每个项目只有一个main函数
+func main() {
+	var name string
+	 name = "s"
+   var p := name  //直接声明并赋值（必须是初次声明才有冒号）
+
+  var a,b,c = 1,2,3
+	var d,e,f int
+   
+	var(k int l string)
+
+	var m,n,o = "a",1,true
+
+	h,i,j := 1,2,3
+
+    fmt.Printf("Hello word",p,a,b,c,d,e,f)
+}
